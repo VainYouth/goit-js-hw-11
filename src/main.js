ffee16c8db9a5e75a50d2b56fbff626cabd6103a
Yinest <80900691+VainYouth@users.noreply.github.com>
2025-12-02 00:54:21 +0300

    fix main js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import { getImagesByQuery } from './js/pixabay-api.js';
-import { clearGallery, showLoader, hideLoader, makeMarkup, } from './js/render-functions.js';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+import { clearGallery, showLoader, makeMarkup } from './js/render-functions.js';
 
 // ===================================================================
-// ЄЛЕМЕНТИ
+// БАЗА
 const form = document.querySelector('.form');
 const submitBtn = document.querySelector('button[type=submit]');
 const input = document.querySelector('input[name="search-text"]');
@@ -16,45 +14,24 @@ submitBtn.disabled = true;
 input.addEventListener('input', evt => {
   submitBtn.disabled = evt.target.value.trim() === '';
 });
-
 // ===================================================================
 // ПОДІЯ сабміт
 form.addEventListener('submit', evt => {
   evt.preventDefault();
   showLoader();
 
-  const searchWord = input.value.trim();
-
+  const searchWord = document
+    .querySelector('input[name="search-text"]')
+    .value.trim();
   if (!searchWord) {
-    hideLoader();
     return;
   }
 
   clearGallery();
 
   getImagesByQuery(searchWord)
-    .then(data => {
-      if (!data.hits || data.hits.length === 0) {
-        iziToast.warning({
-          title: 'Немає результатів',
-          message: `Зображення за запитом "${searchWord}" не знайдено.`,
-          position: 'topRight',
-        });
-        return;
-      }
-
-      makeMarkup(data.hits);
-    })
-    .catch(() => {
-      iziToast.error({
-        title: 'Помилка',
-        message: 'Сталася помилка під час отримання даних. Спробуйте ще раз.',
-        position: 'topRight',
-      });
-    })
-    .finally(() => {
-      hideLoader();
-      form.reset();
-      submitBtn.disabled = true;
-    });
+    .then(makeMarkup)
+    .catch(error => console.log(error));
+  form.reset();
+  submitBtn.disabled = true;
 });
